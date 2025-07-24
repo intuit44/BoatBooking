53d1a5328f2a2bbedfc8df99a6d3a7e5834bb33c
Winston <wesparza888@icloud.com.com>
2025-07-24 14:04:00 -0400

    Refactoring the Jest configuration for the mobile app to improve test performance and reliability.

--- mobile-app/jest.config.js
+++ mobile-app/jest.config.js
@@ -2,16 +2,30 @@
 
 module.exports = {
   preset: 'jest-expo',
+  testEnvironment: 'jest-expo/environment/jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|expo(nent)?/.*|expo/.*|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|aws-amplify|@aws-amplify/.*|uuid|react-native-url-polyfill)'
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|aws-amplify|@aws-amplify/.*|uuid|react-native-url-polyfill)'
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testEnvironment: 'jest-expo/environments/jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: ['babel-preset-expo']
+      }
+    ]
+  },
+  // Para manejar correctamente los módulos de Expo
+  moduleNameMapper: {
+    '^expo$': '<rootDir>/node_modules/expo/build/index.js'
+  },
+  // Variables globales
+  globals: {
+    __DEV__: true
+  },
   // Para manejar timers correctamente
   fakeTimers: {
     enableGlobally: true
-  },
-  roots: ['<rootDir>'],
-  moduleDirectories: ['node_modules', '<rootDir>'],
+  }
 };
