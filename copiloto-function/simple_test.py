14ddd32138e21c80333c1e81b5371394f08a6147
Winston <wesparza888@icloud.com.com>
2025-09-25 04:36:38 -0400

    Modiificacion function_app.py para agregar manejo de errores y logging y openapi_copiloto_local

---
+++ copiloto-function/simple_test.py
@@ -0,0 +1,4 @@
+import sys
+print("Script ejecutado correctamente")
+print("Argumentos:", sys.argv[1:])
+sys.exit(0)
---
+++ copiloto-function/utf8_test.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+import sys
+print("¡Hola con acento!")
+print("Argumentos:", sys.argv[1:])
